0b41ff1d7dfb67bf0b3d6c673721f82e0d88d41c
Grant Mills <dormice_manlike_0r@icloud.com>
2026-05-09 16:15:30 -0400

    [ref MBTA-018] now use bouncy castle to render the self signed cert in code and allow local running of the backend for testing purposes.

--- docker-run.sh
+++ docker-run.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 set -e
 
-KEYSTORE_PASS=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32)
-export KEYSTORE_PASSWORD="$KEYSTORE_PASS"
-mkdir -p /app/tls
-keytool -genkeypair -alias mbta \
-  -keyalg RSA -keysize 2048 -validity 365 \
-  -dname "CN=mbta-internal" \
-  -storetype PKCS12 \
-  -keystore /app/tls/keystore.p12 \
-  -storepass "$KEYSTORE_PASS" \
-  -keypass "$KEYSTORE_PASS" \
-  -noprompt
-
 CLASSPATH=$(find /app/lib/ -name '*.jar' | tr '\n' ':' | sed 's/:$//')
 exec java -cp "$CLASSPATH" mbta.actor.MBTAMain
